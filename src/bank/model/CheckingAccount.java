43e473d4764938f6b1edd60c5505fc5878383218
Pedro Mesquita <pedrolucazxmesquita@gmail.com>
2026-05-28 19:58:45 -0300

    feat(model): adiciona conta corrente com limite de cheque especial

---
+++ src/bank/model/CheckingAccount.java
@@ -0,0 +1,33 @@
+package bank.model;
+
+import bank.exception.BankException;
+
+public class CheckingAccount extends Account {
+
+    private final double overdraftLimit;
+
+    public CheckingAccount(int accountNumber, String holderName, double overdraftLimit) {
+        super(accountNumber, holderName);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) throws BankException {
+        if (amount <= 0) {
+            throw new BankException.InvalidAmount("O valor do saque deve ser positivo.");
+        }
+        double available = balance + overdraftLimit;
+        if (amount > available) {
+            throw new BankException.InsufficientFunds(available);
+        }
+        balance -= amount;
+    }
+
+    public double getOverdraftLimit() { return overdraftLimit; }
+
+    @Override
+    public String toString() {
+        return super.toString()
+             + String.format(" | Corrente | Limite especial: R$ %.2f", overdraftLimit);
+    }
+}
